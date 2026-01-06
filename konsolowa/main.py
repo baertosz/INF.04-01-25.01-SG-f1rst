8ae9cd2b126757d762031015db8608827151e811
baertosz <bartoszly@protonmail.com>
2026-01-06 19:12:16 +0100

    aplikacja konsolowa finish

--- konsolowa/main.py
+++ konsolowa/main.py
@@ -4,9 +4,46 @@ class OperacjeNaTablicach:
     def __init__(self, rozmiar_tablicy: int):
         self.__rozmiar_tablicy = rozmiar_tablicy
         self.__liczby = [random.randint(a=1, b=1000) for _ in range(rozmiar_tablicy)] 
-        print(self.__liczby)
-
-if __name__ == '__main__':
-    operacje = OperacjeNaTablicach(5)
-#pola/konstruktor √#
     
+    def wyswietl_elementy(self):
+        for i in range(self.__rozmiar_tablicy):
+            print(f"{i}: {self.__liczby[i]}")
+
+    def znajdz_pierwszy(self, szukana_liczba):
+        for i in range(self.__rozmiar_tablicy):
+            if szukana_liczba == self.__liczby[i]: 
+                return i
+            
+        return -1  
+    
+    def wypisz_nieparzyste(self):
+        ilosc = 0
+        for liczba in self.__liczby:
+            if liczba % 2 == 1:
+                print(liczba)
+                ilosc += 1
+
+        return ilosc
+    
+    def srednia(self):
+        suma = 0
+        for liczba in self.__liczby:
+            suma += liczba
+
+        return suma / self.__rozmiar_tablicy
+
+if __name__ == '__main__': 
+    operacje = OperacjeNaTablicach(21)
+    operacje.wyswietl_elementy()
+    
+    wyszukany_indeks = operacje.znajdz_pierwszy(42)
+
+    if wyszukany_indeks != -1:
+        print(f"Wyszukany indeks: {wyszukany_indeks}")
+
+    liczba_nieparzystych = operacje.wypisz_nieparzyste()
+    print(f"Ilość liczb nieparzystych: {liczba_nieparzystych}") 
+
+    srednia = operacje.srednia()
+    print(f"Średnia arytmetyczna tablicy: {srednia}")
+      
